bc66dce4108d8d3bfc0e5d07db26c77c1fceb4a6
peter0618 <peterchoi0618@gmail.com>
2021-03-04 01:13:00 +0900

    비디오 업로드 FORM 만들기

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -33,9 +33,12 @@ function RightMenu(props) {
   } else {
     return (
       <Menu mode={props.mode}>
-        <Menu.Item key="logout">
-          <a onClick={logoutHandler}>Logout</a>
-        </Menu.Item>
+          <Menu.Item key="upload">
+            <a href="/video/upload">Video</a>
+          </Menu.Item>
+          <Menu.Item key="logout">
+            <a onClick={logoutHandler}>Logout</a>
+          </Menu.Item>
       </Menu>
     )
   }
