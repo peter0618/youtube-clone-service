a27b76aca2e0be7aad156bd10c6c34e978c1817b
peter0618 <peterchoi0618@gmail.com>
2021-03-09 01:30:43 +0900

    ffmpeg로 비디오 썸네일 생성하기

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -24,6 +24,9 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState("");
     const [Private, setPrivate] = useState(1); // private: 0, public: 1
     const [Category, setCategory] = useState(2);
+    const [FilePath, setFilePath] = useState("");
+    const [Duration, setDuration] = useState("");
+    const [ThumbnailPath, setThumbnailPath] = useState("");
 
     const onTitleChange = (e) => {
         setVideoTitle(e.currentTarget.value);
@@ -52,6 +55,26 @@ function VideoUploadPage() {
             .then(response => {
                 if(response.data.success) {
                     console.log(response.data);
+
+                    let variable = {
+                        url: response.data.url,
+                        fileName: response.data.fileName
+                    }
+
+                    setFilePath(response.data.url);
+
+                    Axios.post('/api/video/thumbnail', variable)
+                        .then(response => {
+                            if(response.data.success){
+
+                                setDuration(response.data.fileDuration);
+                                setThumbnailPath(response.data.url);
+
+
+                            } else {
+                                alert('썸네일 생성에 실패했습니다.');
+                            }
+                        })
                 } else {
                     alert('비디오 업로드를 실패했습니다.')
                 }
@@ -81,9 +104,12 @@ function VideoUploadPage() {
                     </Dropzone>
 
                     {/* Thumbnail */}
+                    {ThumbnailPath && // ThumbnailPath 가 있는 경우에만 랜더링
                     <div>
-                        <img src="" alt=""/>
+                        <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail"/>
                     </div>
+                    }
+
                 </div>
                 <br/>
                 <br/>
